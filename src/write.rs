cd5be8ced139ad8f94d566ad643b59f106c2d895
yoshrc <immortal836@gmail.com>
2019-02-27 18:27:18 +0900

    Add doc in write module

--- src/write.rs
+++ src/write.rs
@@ -1,7 +1,13 @@
+//! Output to stdout.
+
 #[snippet = "write"]
 #[allow(unused_imports)]
 use std::io::{self, Write, BufWriter, StdoutLock};
 
+// TODO: Add a real example (maybe in marathon match)
+/// Make tons of output to stdout much faster.
+///
+/// See [Qiita article by hatoo](https://qiita.com/hatoo@github/items/fa14ad36a1b568d14f3e#%E3%81%8A%E3%81%BE%E3%81%91).
 #[snippet = "write"]
 pub fn with_stdout<F: FnOnce(BufWriter<StdoutLock>)>(f: F) {
     let stdout = io::stdout();
