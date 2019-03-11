f557cfa5ea574d67bd7255a082c3ef60fd117fc4
yoshrc <immortal836@gmail.com>
2019-03-11 14:41:54 +0900

    Modify emacs script and add README for emacs

---
+++ emacs/README.md
@@ -0,0 +1,22 @@
+# atcoder-snippets for Emacs
+
+## Requirements
+
+- rust-mode
+- yasnippet
+
+## How to use
+
+1. Clone this repository.
+2. Add the path of `emacs/bin` directory under the repository to `$PATH` by editing the login commands file like `~/.profile`.
+3. Log out from your OS and log in again.
+4. Execute:
+   ```
+   (push "/path/to/atcoder-snippets/emacs" load-path)
+   (require 'atcoder-snippets)
+   (setq atcoder-snippets-repository-path "/path/to/atcoder-snippets")
+
+   (require 'yasnippet)
+   (atcoder-snippets-reload)
+   ```
+5. Open a rust file and type a snippet name like `read`. Then `M-x yas-expand`.
--- emacs/atcoder-snippets.el
+++ emacs/atcoder-snippets.el
@@ -1,14 +1,21 @@
-(defvar atcoder-snippets-repository-path)
+(defvar atcoder-snippets-repository-path nil
+  "Path of atcoder-snippets repository.
+Clone https://github.com/yoshrc/rust-atcoder-snippets and set the path of the repository.")
 
-(defvar atcoder-snippets-dir (expand-file-name "~/.emacs.d/snippets/rust-mode"))
+(defvar atcoder-snippets-dir (expand-file-name "~/.emacs.d/snippets/rust-mode")
+  "The directory where snippet files get deployed in.")
 
 (defun atcoder-snippets-reload ()
   (interactive)
+  (when (not atcoder-snippets-repository-path)
+    (error "Set `atcoder-snippets-repository-path`"))
   (let ((cmd (concat "cargo snippet -t vscode | atcoder-snippets-deploy-emacs "
                      (shell-quote-argument atcoder-snippets-dir)))
         (default-directory atcoder-snippets-repository-path))
     (when (/= (call-process "sh" nil '("*atcoder-snippet output*" t) nil
                             "-c" cmd)
               0)
-      (error "Snippet deployment failed")))
+      (error "Snippet deployment failed: see *atcoder-snippet output* buffer")))
   (yas-reload-all))
+
+(provide 'atcoder-snippets)
