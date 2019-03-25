41bf86c251d856c3ece1033afc09141a5f477f66
yoshrc <immortal836@gmail.com>
2019-03-25 14:49:29 +0900

    Improve doc

--- src/cmp.rs
+++ src/cmp.rs
@@ -1,11 +1,26 @@
+//! Comparation and sorting.
+
 // ABC 038 D
 
 #[snippet = "cmp"]
 use std::cmp::{Ord, Ordering};
 
+/// For reversed ordering.
+///
+/// See [Qiita article by hatoo](https://qiita.com/hatoo@github/items/fa14ad36a1b568d14f3e#%E9%80%86%E9%A0%86%E3%81%A7%E3%80%87%E3%80%87%E3%81%99%E3%82%8B%E3%81%AB%E3%81%AF).
+///
+/// # Example
+///
+/// ```
+/// # #[macro_use] extern crate atcoder_snippets;
+/// # use atcoder_snippets::cmp::*;
+/// let mut seq = vec![1, 5, 3, 2, 4];
+/// seq.sort_by_key(|&x| Reverse(x));
+/// assert_eq!(seq, vec![5, 4, 3, 2, 1]);
+/// ```
 #[snippet = "cmp"]
 #[derive(Clone, Copy, PartialEq, Eq)]
-struct Reverse<T>(T);
+pub struct Reverse<T>(pub T);
 
 #[snippet = "cmp"]
 impl<T: PartialOrd> PartialOrd for Reverse<T> {
@@ -22,7 +37,7 @@ impl<T: Ord> Ord for Reverse<T> {
 }
 
 #[snippet = "cmp"]
-trait SortDesc<T> {
+pub trait SortDesc<T> {
     // ABC112 D
     fn sort_desc(&mut self) where T: Ord;
 
@@ -44,9 +59,20 @@ impl<T> SortDesc<T> for [T] {
     }
 }
 
+/// Forcibly makes `PartialOrd` into `Ord`, typically for sorting floating point numbers.
+///
+/// # Example
+///
+/// ```
+/// # #[macro_use] extern crate atcoder_snippets;
+/// # use atcoder_snippets::cmp::*;
+/// let mut seq = vec![1.0, 5.0, 3.0, 2.0, 4.0];
+/// seq.sort_by_key(|&x| Total(x));
+/// assert_eq!(seq, vec![1.0, 2.0, 3.0, 4.0, 5.0]);
+/// ```
 #[snippet = "cmp"]
 #[derive(PartialEq, PartialOrd)]
-struct Total<T: PartialOrd + PartialEq>(T);
+pub struct Total<T: PartialOrd + PartialEq>(pub T);
 
 #[snippet = "cmp"]
 impl<T: PartialOrd + PartialEq> Eq for Total<T> {}
@@ -63,13 +89,6 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_sort_by_reverse() {
-        let mut vec = vec![1,5,2,4,3];
-        vec.sort_by_key(|&x| Reverse(x));
-        assert_eq!(vec, vec![5,4,3,2,1]);
-    }
-
-    #[test]
     fn test_sort_desc() {
         let mut vec = vec![1,5,2,4,3];
         vec.sort_desc();
@@ -81,12 +100,5 @@ mod tests {
         let mut vec = vec!["1", "12345", "12", "1234", "123"];
         vec.sort_desc_by_key(|s| s.len());
         assert_eq!(vec, vec!["12345", "1234", "123", "12", "1"]);
-    }
-
-    #[test]
-    fn test_total() {
-        let mut vec = vec![1.0, 3.0, 2.0, 5.0, 4.0];
-        vec.sort_by_key(|&x| Total(x));
-        assert_eq!(vec, vec![1.0, 2.0, 3.0, 4.0, 5.0]);
     }
 }
