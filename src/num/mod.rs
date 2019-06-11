9ee0dd0cb05e32ca7cb51ed5d374d9b760673c18
yoshrc <immortal836@gmail.com>
2019-06-11 19:00:55 +0900

    Add modp function

--- src/num/mod.rs
+++ src/num/mod.rs
@@ -4,7 +4,7 @@
 // pub type BigDigit = u64;
 
 mod primitives;
-mod mod_p;
+mod modp;
 
 pub use self::primitives::{PrimitiveInteger, PrimitiveUnsigned};
-pub use self::mod_p::{ModP, ModPBase};
+pub use self::modp::{modp, ModP, ModPBase};
