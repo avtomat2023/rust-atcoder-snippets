53c4cbca1f2cc22ac1f23d83c71ed2dc3011be8f
yoshrc <immortal836@gmail.com>
2019-08-11 16:53:02 +0900

    Change the interface of ModP to set the modulus dynamically

--- src/num/modp.rs
+++ src/num/modp.rs
@@ -1,16 +1,24 @@
 //! Arithmetics modulo a prime number.
+//!
+//! Never use this module in multi-threaded programs.
+// 動的なmod設定が必要な問題: ABC137 F
+// 複数のmodを使い分けなければならない問題には対応できない
 
 #[snippet = "modp"]
 pub type ModPBase = u64;
 
+#[snippet = "modp"]
+pub type ModPModulus = u32;
+
 /// The modulus which is a prime number.
 ///
-/// In the contest, change the value of this constant.
+/// In the contest, change the value by `ModP::set_mod` method
+/// before any use of `ModP`.
 /// Typically, the value is `1_000_000_007`.
 #[snippet = "modp"]
-pub const MODULUS: ModPBase = 7;
+static mut MODULUS: ModPBase = 0;
 
-/// A number whose arithmetics is carried modulo `MODULUS`.
+/// A number whose arithmetics is carried modulo a prime number.
 #[snippet = "modp"]
 #[derive(Clone, Copy, PartialEq, Eq)]
 pub struct ModP {
@@ -19,22 +27,69 @@ pub struct ModP {
 
 #[snippet = "modp"]
 impl ModP {
+    /// Sets the modulus.
+    ///
+    /// If `modulus` is not a prime number, returns `Err`.
+    ///
+    /// # Undefined behaviors
+    ///
+    /// If you make another call of `set_mod` after creating `ModP` numbers,
+    /// you must not use the numbers.
+    /// The correctness of calculations using the numbers is not guaranteed.
+    ///
+    /// If you call `set_mod` when two or more threads use `ModP` numbers,
+    /// the correctness of calculations using the numbers is not guaranteed.
+    pub unsafe fn set_mod(modulus: ModPModulus) -> Result<(), String> {
+        if modulus <= 1 {
+            return Err(format!("{} is not a prime number.", modulus));
+        }
+
+        if modulus >= 4 {
+            if modulus % 2 == 0 || modulus % 3 == 0 {
+                return Err(format!("{} is not a prime number.", modulus));
+            }
+            let mut divisor = 5;
+            loop {
+                if divisor * divisor > modulus {
+                    break;
+                }
+                if modulus % divisor == 0 {
+                    return Err(format!("{} is not a prime number.", modulus));
+                }
+                divisor += 2;
+
+                if divisor * divisor > modulus {
+                    break;
+                }
+                if modulus % divisor == 0 {
+                    return Err(format!("{} is not a prime number.", modulus));
+                }
+                divisor += 4;
+            }
+        }
+
+        MODULUS = modulus as ModPBase;
+        Ok(())
+    }
+
     /// Create a number.
     pub fn new(n: ModPBase) -> ModP {
         if !cfg!(test) {
-            assert!(MODULUS != 7, "Set const MODULUS to the value provided by the problem.");
+            assert!(unsafe { MODULUS } != 0,
+                    "Call ModP::set_mod before using ModP.");
         }
-        ModP { base: n % MODULUS }
+        ModP { base: n % unsafe { MODULUS } }
     }
 
-    /// Create a number without taking remainder by `MODULUS`.
+    /// Create a number without taking remainder by the modulus.
     ///
-    /// If `n >= MODULUS`, the correctness of arithmetics is not guaranteed.
+    /// If n is greater than or equal to the modulus,
+    /// the correctness of calculations is not guaranteed.
     pub unsafe fn new_unchecked(n: ModPBase) -> ModP {
         ModP { base: n }
     }
 
-    /// Returns a `ModPBase` satisfying `0 <= x < MODULUS`.
+    /// Returns a `ModPBase` satisfying `0 <= x < modulus`.
     pub fn base(&self) -> ModPBase {
         self.base
     }
@@ -78,7 +133,7 @@ impl ModP {
     /// ```
     pub fn inv(self) -> ModP {
         assert!(self.base() != 0);
-        self.pow(MODULUS - 2)
+        self.pow(unsafe { MODULUS } - 2)
     }
 }
 
@@ -105,14 +160,14 @@ impl std::fmt::Debug for ModP {
 #[snippet = "modp"]
 impl PartialEq<ModPBase> for ModP {
     fn eq(&self, other: &ModPBase) -> bool {
-        self.base() == other % MODULUS
+        self.base() == other % unsafe { MODULUS }
     }
 }
 
 #[snippet = "modp"]
 impl PartialEq<ModP> for ModPBase {
     fn eq(&self, other: &ModP) -> bool {
-        self % MODULUS == other.base() % MODULUS
+        self % unsafe { MODULUS } == other.base() % unsafe { MODULUS }
     }
 }
 
@@ -121,7 +176,8 @@ impl std::ops::Add for ModP {
     type Output = ModP;
 
     fn add(self, rhs: ModP) -> ModP {
-        ModP { base: (self.base() + rhs.base() % MODULUS) % MODULUS }
+        let m = unsafe { MODULUS };
+        ModP { base: (self.base() + rhs.base() % m) % m }
     }
 }
 
@@ -162,7 +218,7 @@ impl std::ops::Neg for ModP {
     type Output = ModP;
 
     fn neg(self) -> ModP {
-        ModP::new(MODULUS - self.base())
+        ModP::new(unsafe { MODULUS } - self.base())
     }
 }
 
@@ -212,7 +268,8 @@ impl std::ops::Mul for ModP {
     type Output = ModP;
 
     fn mul(self, rhs: ModP) -> ModP {
-        ModP { base: self.base() * (rhs.base() % MODULUS) % MODULUS }
+        let m = unsafe { MODULUS };
+        ModP { base: self.base() * (rhs.base() % m) % m }
     }
 }
 
@@ -343,13 +400,54 @@ mod tests {
     use super::*;
 
     #[test]
+    fn test_set_mod() {
+        unsafe {
+            // small numbers
+            assert!(ModP::set_mod(0).is_err());
+            assert!(ModP::set_mod(1).is_err());
+            assert!(ModP::set_mod(2).is_ok());
+            assert!(ModP::set_mod(3).is_ok());
+            assert!(ModP::set_mod(4).is_err());
+            assert!(ModP::set_mod(5).is_ok());
+            assert!(ModP::set_mod(6).is_err());
+            assert!(ModP::set_mod(7).is_ok());
+            assert!(ModP::set_mod(8).is_err());
+            assert!(ModP::set_mod(9).is_err());
+            assert!(ModP::set_mod(10).is_err());
+
+            // large numbers
+            assert!(ModP::set_mod(10000).is_err());
+            assert!(ModP::set_mod(10001).is_err());
+            assert!(ModP::set_mod(10002).is_err());
+            assert!(ModP::set_mod(10003).is_err());
+            assert!(ModP::set_mod(10004).is_err());
+            assert!(ModP::set_mod(10005).is_err());
+            assert!(ModP::set_mod(10006).is_err());
+            assert!(ModP::set_mod(10007).is_ok());
+            assert!(ModP::set_mod(10008).is_err());
+            assert!(ModP::set_mod(10009).is_ok());
+
+            // square numbers
+            assert!(ModP::set_mod(16).is_err());
+            assert!(ModP::set_mod(7056).is_err());
+
+            // typical prime numbers
+            assert!(ModP::set_mod(998_244_353).is_ok());
+            assert!(ModP::set_mod(1_000_000_007).is_ok());
+        }
+    }
+
+    #[test]
     fn test_new() {
+        unsafe { ModP::set_mod(7).unwrap(); }
         assert_eq!(ModP::new(3), ModP::new(10));
         assert_eq!(modp(3), modp(10));
     }
 
     #[test]
     fn test_pow() {
+        unsafe { ModP::set_mod(7).unwrap(); }
+
         let zero = ModP::new(0);
         assert_eq!(zero.pow(0), ModP::new(1));
         assert_eq!(zero.pow(1), ModP::new(0));
@@ -365,6 +463,7 @@ mod tests {
 
     #[test]
     fn test_inv() {
+        unsafe { ModP::set_mod(7).unwrap(); }
         assert_eq!(ModP::new(1).inv(), ModP::new(1));
         assert_eq!(ModP::new(2).inv(), ModP::new(4));
         assert_eq!(ModP::new(3).inv(), ModP::new(5));
@@ -375,6 +474,7 @@ mod tests {
 
     #[test]
     fn test_partial_eq() {
+        unsafe { ModP::set_mod(7).unwrap(); }
         assert_eq!(ModP::new(3), 10);
         assert_eq!(ModP::new(10), 3);
         assert_eq!(3, ModP::new(10));
@@ -383,6 +483,7 @@ mod tests {
 
     #[test]
     fn test_add() {
+        unsafe { ModP::set_mod(7).unwrap(); }
         assert_eq!(ModP::new(5) + ModP::new(5), ModP::new(3));
         assert_eq!(ModP::new(5) + 5, ModP::new(3));
         assert_eq!(5 + ModP::new(5), ModP::new(3));
@@ -390,12 +491,15 @@ mod tests {
 
     #[test]
     fn test_add_avoiding_overflow() {
+        unsafe { ModP::set_mod(7).unwrap(); }
         assert_eq!(ModP::new(5) + u64::max_value(), ModP::new(6));
         assert_eq!(u64::max_value() + ModP::new(5), ModP::new(6))
     }
 
     #[test]
     fn test_add_assign() {
+        unsafe { ModP::set_mod(7).unwrap(); }
+
         let mut n1 = ModP::new(5);
         n1 += ModP::new(5);
         assert_eq!(n1, ModP::new(3));
@@ -407,6 +511,7 @@ mod tests {
 
     #[test]
     fn test_add_assign_avoiding_overflow() {
+        unsafe { ModP::set_mod(7).unwrap(); }
         let mut n = ModP::new(5);
         n += u64::max_value();
         assert_eq!(n, ModP::new(6));
@@ -414,12 +519,14 @@ mod tests {
 
     #[test]
     fn test_neg() {
+        unsafe { ModP::set_mod(7).unwrap(); }
         assert_eq!(-ModP::new(3), ModP::new(4));
         assert_eq!(-ModP::new(0), ModP::new(0));
     }
 
     #[test]
     fn test_sub() {
+        unsafe { ModP::set_mod(7).unwrap(); }
         assert_eq!(ModP::new(3) - ModP::new(4), ModP::new(6));
         assert_eq!(ModP::new(3) - 4, ModP::new(6));
         assert_eq!(3 - ModP::new(4), ModP::new(6));
@@ -427,11 +534,14 @@ mod tests {
 
     #[test]
     fn test_sub_avoiding_overflow() {
+        unsafe { ModP::set_mod(7).unwrap(); }
         assert_eq!(ModP::new(5) - u64::max_value(), ModP::new(4));
     }
 
     #[test]
     fn test_sub_assign() {
+        unsafe { ModP::set_mod(7).unwrap(); }
+
         let mut n1 = ModP::new(3);
         n1 -= ModP::new(4);
         assert_eq!(n1, ModP::new(6));
@@ -443,6 +553,7 @@ mod tests {
 
     #[test]
     fn test_sub_assign_avoiding_overflow() {
+        unsafe { ModP::set_mod(7).unwrap(); }
         let mut n = ModP::new(5);
         n -= u64::max_value();
         assert_eq!(n, ModP::new(4));
@@ -450,6 +561,7 @@ mod tests {
 
     #[test]
     fn test_mul() {
+        unsafe { ModP::set_mod(7).unwrap(); }
         assert_eq!(ModP::new(5) * ModP::new(5), ModP::new(4));
         assert_eq!(ModP::new(5) * 5, ModP::new(4));
         assert_eq!(5 * ModP::new(5), ModP::new(4));
@@ -457,12 +569,15 @@ mod tests {
 
     #[test]
     fn test_mul_avoiding_overflow() {
+        unsafe { ModP::set_mod(7).unwrap(); }
         assert_eq!(ModP::new(5) * u64::max_value(), ModP::new(5));
         assert_eq!(u64::max_value() * ModP::new(5), ModP::new(5))
     }
 
     #[test]
     fn test_mul_assign() {
+        unsafe { ModP::set_mod(7).unwrap(); }
+
         let mut n1 = ModP::new(5);
         n1 *= ModP::new(5);
         assert_eq!(n1, ModP::new(4));
@@ -474,6 +589,7 @@ mod tests {
 
     #[test]
     fn test_mul_assign_avoiding_overflow() {
+        unsafe { ModP::set_mod(7).unwrap(); }
         let mut n = ModP::new(5);
         n *= u64::max_value();
         assert_eq!(n, ModP::new(5));
@@ -481,6 +597,7 @@ mod tests {
 
     #[test]
     fn test_div() {
+        unsafe { ModP::set_mod(7).unwrap(); }
         assert_eq!(ModP::new(3) / ModP::new(4), ModP::new(6));
         assert_eq!(ModP::new(3) / 4, ModP::new(6));
         assert_eq!(3 / ModP::new(4), ModP::new(6));
@@ -488,6 +605,8 @@ mod tests {
 
     #[test]
     fn test_div_assign() {
+        unsafe { ModP::set_mod(7).unwrap(); }
+
         let mut n1 = ModP::new(3);
         n1 /= ModP::new(4);
         assert_eq!(n1, ModP::new(6));
@@ -499,6 +618,7 @@ mod tests {
 
     #[test]
     fn test_sum() {
+        unsafe { ModP::set_mod(7).unwrap(); }
         let seq: Vec<ModP> = (1..=6).map(|n| ModP::new(n)).collect();
         assert_eq!(seq.iter().sum::<ModP>(), ModP::new(0));
         assert_eq!(seq.into_iter().sum::<ModP>(), ModP::new(0));
@@ -506,6 +626,7 @@ mod tests {
 
     #[test]
     fn test_product() {
+        unsafe { ModP::set_mod(7).unwrap(); }
         let seq: Vec<ModP> = (1..=6).map(|n| ModP::new(n)).collect();
         assert_eq!(seq.iter().product::<ModP>(), ModP::new(6));
         assert_eq!(seq.into_iter().product::<ModP>(), ModP::new(6));
@@ -513,6 +634,7 @@ mod tests {
 
     #[test]
     fn test_read() {
+        unsafe { ModP::set_mod(7).unwrap(); }
         assert_eq!(ModP::read_words(&["10"]), Ok(ModP::new(3)));
     }
 }
