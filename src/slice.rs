38606699bca4954c1778c34352e6a7477845c4ce
yoshrc <immortal836@gmail.com>
2019-09-24 06:51:06 +0900

    Add split_by_gap method to SliceExt

--- src/slice.rs
+++ src/slice.rs
@@ -17,7 +17,6 @@ impl<'a, T, K: Eq, F: Fn(&T) -> K> Iterator for SliceGroupBy<'a, T, K, F> {
             return None;
         }
 
-
         let key = (self.key_fn)(&self.rest[0]);
         let mut end = 1;
         while end < self.rest.len() && (self.key_fn)(&self.rest[end]) == key {
@@ -27,6 +26,33 @@ impl<'a, T, K: Eq, F: Fn(&T) -> K> Iterator for SliceGroupBy<'a, T, K, F> {
         let (left, right) = self.rest.split_at(end);
         self.rest = right;
         Some((key, left))
+    }
+}
+
+// TODO: AGC038 B
+/// An iterator created by [`split_by_gap`](trait.SliceExt.html#tymethod.split_by_gap) method on slices.
+#[snippet = "slice"]
+pub struct SplitByGap<'a, T: 'a, F: Fn(&T, &T) -> bool> {
+    gap_fn: F,
+    rest: &'a [T]
+}
+
+#[snippet = "slice"]
+impl<'a, T, F: Fn(&T, &T) -> bool> Iterator for SplitByGap<'a, T, F> {
+    type Item = &'a [T];
+
+    fn next(&mut self) -> Option<&'a [T]> {
+        if self.rest.is_empty() {
+            return None;
+        }
+
+        let mut r = 1;
+        while r < self.rest.len() && !(self.gap_fn)(&self.rest[r-1], &self.rest[r]) {
+            r += 1;
+        }
+        let (result, rest) = self.rest.split_at(r);
+        self.rest = rest;
+        Some(result)
     }
 }
 
@@ -142,6 +168,26 @@ pub trait SliceExt<T> {
     /// ```
     fn group_by<K: Eq, F: Fn(&T) -> K>(&self, key_fn: F) -> SliceGroupBy<T, K, F>;
 
+    /// Returns an iterator yielding subslices separated by `gap_fn`.
+    ///
+    /// `gap_fn` takes 2 items. When `gap_fn(&self[i-1], &self[i])` is `true`,
+    /// `self` is splitted at `i`.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// # extern crate atcoder_snippets;
+    /// # use atcoder_snippets::slice::*;
+    /// let seq = [1, 2, 3, 1, 2, 1, 1];
+    /// // splits seq into increasing subsequences
+    /// let mut subseqs = seq.split_by_gap(|&a, &b| a > b);
+    /// assert_eq!(subseqs.next(), Some(vec![1, 2, 3].as_slice()));
+    /// assert_eq!(subseqs.next(), Some(vec![1, 2].as_slice()));
+    /// assert_eq!(subseqs.next(), Some(vec![1, 1].as_slice()));
+    /// assert_eq!(subseqs.next(), None);
+    /// ```
+    fn split_by_gap<F: Fn(&T, &T) -> bool>(&self, gap_fn: F) -> SplitByGap<T, F>;
+
     // TODO: ABC103 A
     /// Returns an iterator yielding all permutations of the slice.
     ///
@@ -180,6 +226,10 @@ pub trait SliceExt<T> {
 impl<T> SliceExt<T> for [T] {
     fn group_by<K: Eq, F: Fn(&T) -> K>(&self, key_fn: F) -> SliceGroupBy<T, K, F> {
         SliceGroupBy { key_fn: key_fn, rest: self }
+    }
+
+    fn split_by_gap<F: Fn(&T, &T) -> bool>(&self, gap_fn: F) -> SplitByGap<T, F> {
+        SplitByGap { gap_fn: gap_fn, rest: self }
     }
 
     fn permutations(&self) -> Permutations<T> {
@@ -229,5 +279,28 @@ mod test {
         assert_eq!([0, 0, 0, 0, 0, 0, 0, 0, 0].count_inversions(), 0);
         assert_eq!([0, 1, 2, 3, 4, 5, 6, 7, 8].count_inversions(), 0);
         assert_eq!([2, 2, 2, 1, 1, 1, 0, 0, 0].count_inversions(), 27);
+    }
+
+    #[test]
+    fn test_split_by_gap() {
+        let f = |&a: &i32, &b: &i32| a > b;
+        assert!(Vec::<i32>::new().split_by_gap(|&a, &b| a > b).next().is_none());
+        assert_eq!([0].split_by_gap(f).collect::<Vec<_>>(),
+                   vec![vec![0].as_slice()]);
+        assert_eq!([0, 1].split_by_gap(f).collect::<Vec<_>>(),
+                   vec![vec![0, 1].as_slice()]);
+        assert_eq!([1, 0].split_by_gap(f).collect::<Vec<_>>(),
+                   vec![vec![1].as_slice(), vec![0].as_slice()]);
+        assert_eq!([1, 2, 3].split_by_gap(f).collect::<Vec<_>>(),
+                   vec![vec![1, 2, 3].as_slice()]);
+        assert_eq!([1, 3, 2].split_by_gap(f).collect::<Vec<_>>(),
+                   vec![vec![1, 3].as_slice(), vec![2].as_slice()]);
+        assert_eq!([7, 2, 1, 9, 3, 0, 5, 8, 4, 6].split_by_gap(f).collect::<Vec<_>>(),
+                   vec![vec![7].as_slice(),
+                        vec![2].as_slice(),
+                        vec![1, 9].as_slice(),
+                        vec![3].as_slice(),
+                        vec![0, 5, 8].as_slice(),
+                        vec![4, 6].as_slice()]);
     }
 }
