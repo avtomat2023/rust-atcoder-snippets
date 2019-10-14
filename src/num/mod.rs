97cfff9c66f85a690f3639dbe18ffc104d34fffc
yoshrc <immortal836@gmail.com>
2019-10-15 02:00:55 +0900

    Implement gcd and bezout only for 64-bit integers

--- src/num/mod.rs
+++ src/num/mod.rs
@@ -6,5 +6,5 @@
 mod primitives;
 mod modp;
 
-pub use self::primitives::{PrimitiveInteger, PrimitiveUnsigned};
+pub use self::primitives::{PrimitiveInteger, PrimitiveUnsigned, gcd, bezout};
 pub use self::modp::{modp, ModP, ModPBase};
