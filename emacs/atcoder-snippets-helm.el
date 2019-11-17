e7814ec47b79991c1df509ea2a0dc56ea0e6ecab
yoshrc <immortal836@gmail.com>
2019-11-18 00:07:07 +0900

    Auto-paste dependencies for emacs

--- emacs/atcoder-snippets-helm.el
+++ emacs/atcoder-snippets-helm.el
@@ -1,11 +1,14 @@
 (require 'atcoder-snippets)
 (require 'helm)
 
+(require 'dash)
+(require 'ht)
+
 (defun atcoder-snippets-helm-insert ()
   (interactive)
-  (let* ((names (atcoder-snippets-names))
-         (inserted (atcoder-snippets-list-inserted-snippets))
-         (candidates (-filter (lambda (name) (not (gethash name inserted))) names)))
+  (let* ((names (atcoder-snippets--names-sorted-alphabetically))
+         (inserteds (atcoder-snippets--to-hashset (atcoder-snippets--inserted-snippets)))
+         (candidates (--filter (not (ht-contains? inserteds it)) names)))
     (if (not candidates)
         (message "All snippets are already inserted.")
       (let ((source `((name . "Snippets not yet pasted")
