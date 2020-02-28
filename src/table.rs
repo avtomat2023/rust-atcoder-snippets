d947b08b635ad08dbabeb64bf38423d2d02ccf32
yoshrc <immortal836@gmail.com>
2020-02-29 00:16:59 +0900

    Add adjacent_8_indices method to Table

--- src/table.rs
+++ src/table.rs
@@ -62,8 +62,68 @@ impl<T> Table<T> {
         self.inner.first().map_or(0, |row| row.len())
     }
 
+    /// Checks the index is in range.
+    ///
+    /// # Example
+    ///
+    /// ```edition2018
+    /// # use atcoder_snippets::table::*;
+    /// let table = table![5,5];
+    /// assert_eq!(table.in_range((4, 4)), true);
+    /// assert_eq!(table.in_range((4, 5)), false);
+    /// ```
+    pub fn in_range(&self, (y, x): (usize, usize)) -> bool {
+        y < self.row_count() && x < self.col_count()
+    }
+
     pub fn rows(&self) -> TableRows<T> {
         TableRows { table: self, index: 0 }
+    }
+
+    // Ant Book p. 35
+    // TODO: Maybe out-of-range should not be treated as an error.
+    /// Indices of enclosing 8 cells in the dictionary order.p
+    ///
+    /// Useful for traversing a grid graph.
+    ///
+    /// If the given index is out of range, returns `None`.
+    ///
+    /// # Example
+    ///
+    /// ```edition2018
+    /// # use atcoder_snippets::table::*;
+    /// let table = table![5,5];
+    /// assert_eq!(table.adjacent_8_indices((3,2)),
+    ///            Some(vec![(2,1), (2,2), (2,3), (3,1), (3,3), (4,1), (4,2), (4,3)]));
+    /// assert_eq!(table.adjacent_8_indices((4,3)),
+    ///            Some(vec![(3,2), (3,3), (3,4), (4,2), (4,4)]));
+    /// ```
+    pub fn adjacent_8_indices(&self, (y, x): (usize, usize)) -> Option<Vec<(usize, usize)>> {
+        self.in_range((y, x)).then_with(|| {
+            let mut result = Vec::new();
+            let xs = x.saturating_sub(1) ..= std::cmp::min(x+1, self.col_count()-1);
+
+            if y > 0 {
+                for x in xs.clone() {
+                    result.push((y-1, x));
+                }
+            }
+
+            if x > 0 {
+                result.push((y, x-1));
+            }
+            if x < self.col_count()-1 {
+                result.push((y, x+1));
+            }
+
+            if y < self.row_count()-1 {
+                for x in xs {
+                    result.push((y+1, x));
+                }
+            }
+
+            result
+        })
     }
 
     // ABC005 D
@@ -188,5 +248,59 @@ mod tests {
         let table3 = table![0; 3,2];
         assert_eq!(table3.row_count(), 3);
         assert_eq!(table3.col_count(), 2);
+    }
+
+    #[test]
+    fn test_adjacent_8_indices_trivial() {
+        let empty_table: Table<i32> = table![];
+        assert_eq!(empty_table.adjacent_8_indices((0,0)), None);
+        let singleton_table = table![0; 1,1];
+        assert_eq!(singleton_table.adjacent_8_indices((0,0)), Some(vec![]));
+    }
+
+    #[test]
+    fn test_adjacent_8_indices_row() {
+        let table = table![0; 1,3];
+        assert_eq!(table.adjacent_8_indices((0,0)), Some(vec![(0,1)]));
+        assert_eq!(table.adjacent_8_indices((0,1)), Some(vec![(0,0), (0,2)]));
+        assert_eq!(table.adjacent_8_indices((0,2)), Some(vec![(0,1)]));
+        assert_eq!(table.adjacent_8_indices((0,3)), None);
+        assert_eq!(table.adjacent_8_indices((1,0)), None);
+    }
+
+    #[test]
+    fn test_adjacent_8_indices_column() {
+        let table = table![0; 3,1];
+        assert_eq!(table.adjacent_8_indices((0,0)), Some(vec![(1,0)]));
+        assert_eq!(table.adjacent_8_indices((1,0)), Some(vec![(0,0), (2,0)]));
+        assert_eq!(table.adjacent_8_indices((2,0)), Some(vec![(1,0)]));
+        assert_eq!(table.adjacent_8_indices((3,0)), None);
+        assert_eq!(table.adjacent_8_indices((0,1)), None);
+    }
+
+    #[test]
+    fn test_adjacent_8_indices_2x2() {
+        let table = table![0; 2,2];
+        assert_eq!(table.adjacent_8_indices((0,0)), Some(vec![(0,1), (1,0), (1,1)]));
+        assert_eq!(table.adjacent_8_indices((0,1)), Some(vec![(0,0), (1,0), (1,1)]));
+        assert_eq!(table.adjacent_8_indices((1,0)), Some(vec![(0,0), (0,1), (1,1)]));
+        assert_eq!(table.adjacent_8_indices((1,1)), Some(vec![(0,0), (0,1), (1,0)]));
+    }
+
+    #[test]
+    fn test_adjacent_8_indices_3x3() {
+        let table = table![0; 3,3];
+        let check = |y: usize, x: usize| {
+            table.adjacent_8_indices((y, x)).unwrap()
+        };
+        assert_eq!(check(0,0), vec![(0,1), (1,0), (1,1)]);
+        assert_eq!(check(0,1), vec![(0,0), (0,2), (1,0), (1,1), (1,2)]);
+        assert_eq!(check(0,2), vec![(0,1), (1,1), (1,2)]);
+        assert_eq!(check(1,0), vec![(0,0), (0,1), (1,1), (2,0), (2,1)]);
+        assert_eq!(check(1,1), vec![(0,0), (0,1), (0,2), (1,0), (1,2), (2,0), (2,1), (2,2)]);
+        assert_eq!(check(1,2), vec![(0,1), (0,2), (1,1), (2,1), (2,2)]);
+        assert_eq!(check(2,0), vec![(1,0), (1,1), (2,1)]);
+        assert_eq!(check(2,1), vec![(1,0), (1,1), (1,2), (2,0), (2,2)]);
+        assert_eq!(check(2,2), vec![(1,1), (1,2), (2,1)]);
     }
 }
