a819688645f907acf60c5b43b48bec62f1658ea3
yoshrc <immortal836@gmail.com>
2020-02-29 17:46:29 +0900

    Docment and refactor

--- src/table.rs
+++ src/table.rs
@@ -17,6 +17,32 @@ pub struct Table<T> {
     inner: Vec<Vec<T>>
 }
 
+pub struct TableIndicesIterator {
+    height: usize,
+    width: usize,
+    y: usize,
+    x: usize
+}
+
+impl Iterator for TableIndicesIterator {
+    type Item = (usize, usize);
+
+    fn next(&mut self) -> Option<(usize, usize)> {
+        if self.x < self.width {
+            self.x += 1;
+            return Some((self.y, self.x-1))
+        }
+
+        if self.y+1 < self.height {
+            self.y += 1;
+            self.x = 1;
+            return Some((self.y, 0))
+        }
+
+        None
+    }
+}
+
 /// An iterator created by [`Table::rows`](struct.Table.html#method.rows).
 pub struct TableRows<'a, T: 'a> {
     table: &'a Table<T>,
@@ -37,6 +63,24 @@ impl<'a, T: 'a> Iterator for TableRows<'a, T> {
     }
 }
 
+/*
+pub trait TableRangeBounds {
+    fn y_bounds(&self) -> (std::ops::Bound<usize>, std::ops::Bound<usize>);
+    fn x_bounds(&self) -> (std::ops::Bound<usize>, std::ops::Bound<usize>);
+}
+
+// conflicting implementation
+impl<T1: std::ops::RangeBounds<usize>, T2: std::ops::RangeBounds<usize>> TableRangeBounds for (T1, T2) {
+    fn y_bounds(&self) -> (std::ops::Bound<usize>, std::ops::Bound<usize>) {
+        (self.0.start_bound().cloned(), self.0.end_bound().cloned())
+    }
+
+    fn x_bounds(&self) -> (std::ops::Bound<usize>, std::ops::Bound<usize>) {
+        (self.1.start_bound().cloned(), self.1.end_bound().cloned())
+    }
+}
+*/
+
 // ABC005 D
 /// A table created by [`Table::accumulate`](struct.Table.html#method.accumulate).
 pub struct CumulativeTable<T, F1, F2> {
@@ -54,25 +98,61 @@ impl<T> Table<T> {
             .then_with(|| Table { inner: rows })
     }
 
+    /// Create a new table from rows without shape checking.
+    pub unsafe fn from_rows_unchecked(rows: Vec<Vec<T>>) -> Table<T> {
+        Table { inner: rows }
+    }
+
+    /// Number of rows.
     pub fn height(&self) -> usize {
         self.inner.len()
     }
 
+    /// Number of columns.
     pub fn width(&self) -> usize {
         self.inner.first().map_or(0, |row| row.len())
+    }
+
+    /// `(height, column)`.
+    pub fn shape(&self) -> (usize, usize) {
+        (self.height(), self.width())
+    }
+
+    /// Creates an iterator yielding table indices as `(y, x)` in the dictionary order.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// # #[macro_use] extern crate atcoder_snippets;
+    /// # use atcoder_snippets::table::*;
+    /// let table = table![0; 2,3];
+    /// let indices: Vec<(usize, usize)> = table.indices().collect();
+    /// assert_eq!(indices, vec![
+    ///     (0, 0), (0, 1), (0, 2),
+    ///     (1, 0), (1, 1), (1, 2)
+    /// ]);
+    /// ```
+    pub fn indices(&self) -> TableIndicesIterator {
+        TableIndicesIterator {
+            height: self.height(),
+            width: self.width(),
+            y: if self.width() == 0 { self.height() } else { 0 },
+            x: 0
+        }
     }
 
     /// Checks the index is in range.
     ///
     /// # Example
     ///
-    /// ```edition2018
-    /// # use atcoder_snippets::table::*;
-    /// let table = table![5,5];
-    /// assert_eq!(table.in_range((4, 4)), true);
-    /// assert_eq!(table.in_range((4, 5)), false);
     /// ```
-    pub fn in_range(&self, (y, x): (usize, usize)) -> bool {
+    /// # #[macro_use] extern crate atcoder_snippets;
+    /// # use atcoder_snippets::table::*;
+    /// let table = table![0; 5,5];
+    /// assert_eq!(table.inside((4, 4)), true);
+    /// assert_eq!(table.inside((4, 5)), false);
+    /// ```
+    pub fn inside(&self, (y, x): (usize, usize)) -> bool {
         y < self.height() && x < self.width()
     }
 
@@ -90,18 +170,19 @@ impl<T> Table<T> {
     ///
     /// # Example
     ///
-    /// ```edition2018
+    /// ```
+    /// # #[macro_use] extern crate atcoder_snippets;
     /// # use atcoder_snippets::table::*;
-    /// let table = table![5,5];
-    /// assert_eq!(table.adjacent_8_indices((3,2)),
+    /// let table = table![0; 5,5];
+    /// assert_eq!(table.adjacent_indices_8((3,2)),
     ///            Some(vec![(2,1), (2,2), (2,3), (3,1), (3,3), (4,1), (4,2), (4,3)]));
-    /// assert_eq!(table.adjacent_8_indices((4,3)),
+    /// assert_eq!(table.adjacent_indices_8((4,3)),
     ///            Some(vec![(3,2), (3,3), (3,4), (4,2), (4,4)]));
     /// ```
-    pub fn adjacent_8_indices(&self, (y, x): (usize, usize)) -> Option<Vec<(usize, usize)>> {
-        self.in_range((y, x)).then_with(|| {
+    pub fn adjacent_indices_8(&self, (y, x): (usize, usize)) -> Option<Vec<(usize, usize)>> {
+        self.inside((y, x)).then_with(|| {
             let mut result = Vec::new();
-            let xs = x.saturating_sub(1) ..= std::cmp::min(x+1, self.width()-1);
+            let xs = x.saturating_sub(1) .. std::cmp::min(x+1, self.width()-1)+1;
 
             if y > 0 {
                 for x in xs.clone() {
@@ -127,6 +208,26 @@ impl<T> Table<T> {
     }
 
     // ABC005 D
+    /// Creates a cumulative table that can handle 2-dimentional range sum queries, etc.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// # #[macro_use] extern crate atcoder_snippets;
+    /// # use atcoder_snippets::table::*;
+    /// let rows = vec![
+    ///     vec![0,    1,    2,    3,    4],
+    ///     vec![0,   10,   20,   30,   40],
+    ///     vec![0,  100,  200,  300,  400],
+    ///     vec![0, 1000, 2000, 3000, 4000],
+    /// ];
+    /// let table = Table::from_rows(rows).unwrap();
+    /// let cumulative = table.accumulate(0, |&a,&b| a+b, |&a,&b| a-b);
+    /// // Calculates the sum of this subtable:
+    /// // [[ 20,  30,  40],
+    /// //  [200, 300, 400]]
+    /// assert_eq!(cumulative.query_yx(1..=2, 2..).unwrap(), 990)
+    /// ```
     pub fn accumulate<F1, F2>(&self, identity: T, op: F1, op_inv: F2)
                               -> CumulativeTable<T, F1, F2>
     where
@@ -157,7 +258,11 @@ impl<T> Table<T> {
 }
 
 impl<T, F1: Fn(&T, &T) -> T, F2: Fn(&T, &T) -> T> CumulativeTable<T, F1, F2> {
-    pub fn query(&self, y: std::ops::Range<usize>, x: std::ops::Range<usize>) -> Option<T> {
+    pub fn query(&self, range: std::ops::Range<(usize, usize)>) -> Option<T> {
+        self.query_yx(range.start.0 .. range.end.0, range.start.1 .. range.end.1)
+    }
+
+    pub fn query_yx(&self, y: std::ops::Range<usize>, x: std::ops::Range<usize>) -> Option<T> {
         if y.start > y.end || y.end > self.inner.height() - 1 {
             return None;
         }
@@ -182,25 +287,27 @@ impl<T, F1: Fn(&T, &T) -> T, F2: Fn(&T, &T) -> T> CumulativeTable<T, F1, F2> {
 ///
 /// # Example
 ///
-/// ```edition2018
+/// ```
+/// # #[macro_use] extern crate atcoder_snippets;
 /// # use atcoder_snippets::table::*;
 /// // Makes a table with 3 rows and 2 columns, filled by 0.
 /// let table1 = table![0; 3,2];
-/// assert_eq!(table1.rows_count(), 3);
-/// assert_eq!(table1.cols_count(), 2);
+/// assert_eq!(table1.height(), 3);
+/// assert_eq!(table1.width(), 2);
+///
 /// // Makes an empty table.
-/// let table2 = table![];
-/// assert_eq!(table2.rows_count(), 0);
-/// assert_eq!(table2.cols_count(), 0);
+/// let table2: Table<i32> = table![];
+/// assert_eq!(table2.height(), 0);
+/// assert_eq!(table2.width(), 0);
 /// ```
 #[macro_export]
 macro_rules! table {
     () => {
-        Table { inner: Vec::new() }
+        unsafe { Table::from_rows_unchecked(Vec::new()) }
     };
 
     ($element:expr ; $rows:expr , $cols:expr) => {
-        Table { inner: vec![vec![$element; $cols]; $rows] }
+        unsafe { Table::from_rows_unchecked(vec![vec![$element; $cols]; $rows]) }
     };
 }
 
@@ -251,47 +358,72 @@ mod tests {
     }
 
     #[test]
-    fn test_adjacent_8_indices_trivial() {
+    fn test_indices() {
+        fn indices(table: &Table<i32>) -> Vec<(usize, usize)> {
+            table.indices().collect()
+        }
+
+        let table1: Table<i32> = table![];
+        assert_eq!(indices(&table1), vec![]);
+        let table2 = table![0; 3,0];
+        assert_eq!(indices(&table2), vec![]);
+        let table3 = table![0; 3,2];
+        assert_eq!(indices(&table3), vec![(0,0), (0,1), (1,0), (1,1), (2,0), (2,1)]);
+    }
+
+    #[test]
+    fn test_indices_exhausted() {
+        let mut indices = table![0; 2,2].indices();
+        assert_eq!(indices.next(), Some((0, 0)));
+        assert_eq!(indices.next(), Some((0, 1)));
+        assert_eq!(indices.next(), Some((1, 0)));
+        assert_eq!(indices.next(), Some((1, 1)));
+        assert_eq!(indices.next(), None);
+        assert_eq!(indices.next(), None);
+    }
+
+    #[test]
+    fn test_adjacent_indices_8_trivial() {
         let empty_table: Table<i32> = table![];
-        assert_eq!(empty_table.adjacent_8_indices((0,0)), None);
+        assert_eq!(empty_table.adjacent_indices_8((0,0)), None);
         let singleton_table = table![0; 1,1];
-        assert_eq!(singleton_table.adjacent_8_indices((0,0)), Some(vec![]));
+        assert_eq!(singleton_table.adjacent_indices_8((0,0)), Some(vec![]));
     }
 
     #[test]
-    fn test_adjacent_8_indices_row() {
+    fn test_adjacent_indices_8_row() {
         let table = table![0; 1,3];
-        assert_eq!(table.adjacent_8_indices((0,0)), Some(vec![(0,1)]));
-        assert_eq!(table.adjacent_8_indices((0,1)), Some(vec![(0,0), (0,2)]));
-        assert_eq!(table.adjacent_8_indices((0,2)), Some(vec![(0,1)]));
-        assert_eq!(table.adjacent_8_indices((0,3)), None);
-        assert_eq!(table.adjacent_8_indices((1,0)), None);
+        assert_eq!(table.adjacent_indices_8((0,0)), Some(vec![(0,1)]));
+        assert_eq!(table.adjacent_indices_8((0,1)), Some(vec![(0,0), (0,2)]));
+        assert_eq!(table.adjacent_indices_8((0,2)), Some(vec![(0,1)]));
+        assert_eq!(table.adjacent_indices_8((0,3)), None);
+        assert_eq!(table.adjacent_indices_8((1,0)), None);
     }
 
     #[test]
-    fn test_adjacent_8_indices_column() {
+    fn test_adjacent_indices_8_column() {
         let table = table![0; 3,1];
-        assert_eq!(table.adjacent_8_indices((0,0)), Some(vec![(1,0)]));
-        assert_eq!(table.adjacent_8_indices((1,0)), Some(vec![(0,0), (2,0)]));
-        assert_eq!(table.adjacent_8_indices((2,0)), Some(vec![(1,0)]));
-        assert_eq!(table.adjacent_8_indices((3,0)), None);
-        assert_eq!(table.adjacent_8_indices((0,1)), None);
+        assert_eq!(table.adjacent_indices_8((0,0)), Some(vec![(1,0)]));
+        assert_eq!(table.adjacent_indices_8((1,0)), Some(vec![(0,0), (2,0)]));
+        assert_eq!(table.adjacent_indices_8((2,0)), Some(vec![(1,0)]));
+        assert_eq!(table.adjacent_indices_8((3,0)), None);
+        assert_eq!(table.adjacent_indices_8((0,1)), None);
     }
 
     #[test]
-    fn test_adjacent_8_indices_2x2() {
+    fn test_adjacent_indices_8_2x2() {
         let table = table![0; 2,2];
-        assert_eq!(table.adjacent_8_indices((0,0)), Some(vec![(0,1), (1,0), (1,1)]));
-        assert_eq!(table.adjacent_8_indices((0,1)), Some(vec![(0,0), (1,0), (1,1)]));
-        assert_eq!(table.adjacent_8_indices((1,0)), Some(vec![(0,0), (0,1), (1,1)]));
-        assert_eq!(table.adjacent_8_indices((1,1)), Some(vec![(0,0), (0,1), (1,0)]));
+        assert_eq!(table.adjacent_indices_8((0,0)), Some(vec![(0,1), (1,0), (1,1)]));
+        assert_eq!(table.adjacent_indices_8((0,1)), Some(vec![(0,0), (1,0), (1,1)]));
+        assert_eq!(table.adjacent_indices_8((1,0)), Some(vec![(0,0), (0,1), (1,1)]));
+        assert_eq!(table.adjacent_indices_8((1,1)), Some(vec![(0,0), (0,1), (1,0)]));
     }
 
     #[test]
-    fn test_adjacent_8_indices_3x3() {
+    fn test_adjacent_indices_8_3x3() {
         let table = table![0; 3,3];
         let check = |y: usize, x: usize| {
-            table.adjacent_8_indices((y, x)).unwrap()
+            table.adjacent_indices_8((y, x)).unwrap()
         };
         assert_eq!(check(0,0), vec![(0,1), (1,0), (1,1)]);
         assert_eq!(check(0,1), vec![(0,0), (0,2), (1,0), (1,1), (1,2)]);
